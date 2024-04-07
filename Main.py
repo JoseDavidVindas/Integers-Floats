33bba78ea6b866ea3b3b19a178934c4072f90c99
JoseDavidVindas <jose.chavarria34@ulatina.net>
2024-04-07 13:42:45 -0600

    Se agrega el proyecto

---
+++ Main.py
@@ -0,0 +1,8 @@
+num1_str = input("Ingresa el primer número: ")
+num2_str = input("Ingresa el segundo número: ")
+num1 = float(num1_str)
+num2 = float(num2_str)
+resultado = num1 + num2
+tipo_resultado = type(resultado)
+print("El resultado de la suma es:", resultado)
+print("El tipo de dato del resultado es:", tipo_resultado)
